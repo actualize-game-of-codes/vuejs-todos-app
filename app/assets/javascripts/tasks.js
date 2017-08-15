1a8b5d8e98cbc7f8d9b7c0a38b17b220ab369691
Peter Jang <peter.x.jang@gmail.com>
2017-08-14 20:53:46 -0500

    Show tasks using v-for

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -3,7 +3,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      tasks: [
+        {text: 'Take out the garbage', completed: false},
+        {text: 'Make the bed', completed: false},
+        {text: 'Mow the lawn', completed: false}
+      ]
     },
     mounted: function() {
 
